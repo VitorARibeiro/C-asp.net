2fba41da40d81cbdb56b577af6928cba2c27ea3a
VitorARibeiro <Vitorribeiro20022@gmail.com>
2024-04-01 14:07:25 +0100

    turorial1

---
+++ Tutorial1/Tutorial1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Tutorial1.Models;
+
+namespace Tutorial1.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        HomeModel Home = new HomeModel();
+        Home.Nome = "Vitor";
+        Home.Email = "vitor@gmail.com";
+        
+        return View(Home);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Tutorial1/Tutorial1/Models/HomeModel.cs
@@ -0,0 +1,7 @@
+namespace Tutorial1.Models;
+
+public class HomeModel
+{
+    public string Nome { set; get; }
+    public string Email { set; get; }
+}
